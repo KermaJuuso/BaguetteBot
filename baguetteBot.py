c600483fa058d74f54b3754a98b81e8da429e895
Juuso <juusotoivanen03@gmail.com>
2025-01-31 09:03:41 +0200

    add group restriction

--- baguetteBot.py
+++ baguetteBot.py
@@ -1,3 +1,8 @@
+"""
+This bot is used for a community to stay updatet for the
+baguette flavours of each day. A person must visit the Bitti cafe
+and update the bot by /add command.
+"""
 from telegram import Update
 from telegram.ext import Updater, CommandHandler, CallbackContext
 from datetime import datetime
@@ -8,7 +13,23 @@ load_dotenv()
 TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
 baguettes = [] # List of (flavour, date) tuples
 
+ALLOWED_GROUPS = {-1002468965180, 5978668914} #The baguette group
 
+
+def group_restricted(func):
+    """
+    Currently only allows one group and dev
+    """
+    def wrapper(update: Update, context: CallbackContext, *args, **kwargs):
+        chat_id = update.effective_chat.id
+        if chat_id not in ALLOWED_GROUPS:
+            update.message.reply_text("Nope!")
+            return
+        return func(update, context, *args, **kwargs)
+    return wrapper
+
+
+@group_restricted
 def start(update: Update, context: CallbackContext) -> None:
     update.message.reply_text("Hei, olen PatonkiBotti \n"
                               "Komennot:\n"
@@ -22,6 +43,7 @@ def remove_old_baguettes():
     baguettes = [(flavour, date) for flavour, date in baguettes if date == today]
 
 
+@group_restricted
 def add_baguette(update: Update, context: CallbackContext) -> None:
     if not context.args:
         update.message.reply_text("Käytä: /add [patonki]")
@@ -34,6 +56,8 @@ def add_baguette(update: Update, context: CallbackContext) -> None:
     baguettes.append((flavour, today))
     update.message.reply_text(f"Lisättiin patonki #{len(baguettes)}: {flavour} ({today})")
 
+
+@group_restricted
 def del_baguette(update: Update, context: CallbackContext) -> None:
     if not context.args or not context.args[0].isdigit():
         update.message.reply_text("Käytä: /del [patonki numero]")
@@ -48,6 +72,8 @@ def del_baguette(update: Update, context: CallbackContext) -> None:
     else:
         update.message.reply_text("Väärä patonki numero.")
 
+
+@group_restricted
 def list_baguettes(update: Update, context: CallbackContext) -> None:
     remove_old_baguettes()
     today = datetime.now().date()
@@ -61,6 +87,7 @@ def list_baguettes(update: Update, context: CallbackContext) -> None:
     update.message.reply_text(message)
 
 
+@group_restricted
 def del_all_baguettes(update: Update, context: CallbackContext) -> None:
     global baguettes
     if baguettes:
@@ -68,6 +95,7 @@ def del_all_baguettes(update: Update, context: CallbackContext) -> None:
         update.message.reply_text("Kaikki patongit poistettu!")
     else:
         update.message.reply_text("Ei patonkeja poistettavaksi.")
+
 
 def main():
     updater = Updater(TOKEN, use_context=True)
